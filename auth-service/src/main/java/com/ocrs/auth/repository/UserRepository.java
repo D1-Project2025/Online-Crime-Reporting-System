ce7c6e7fa8897d90292fb750f2aba8f7a75c34c6
ghagevaibhav <ghagevaibhav@gmail.com>
2026-01-26 21:50:14 +0530

    added repository layer

---
+++ auth-service/src/main/java/com/ocrs/auth/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.ocrs.auth.repository;
+
+import com.ocrs.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+        Optional<User> findByEmail(String email);
+
+        boolean existsByEmail(String email);
+
+        boolean existsByAadhaarNumber(String aadhaarNumber);
+
+        List<User> findByIsActiveTrue();
+
+        List<User> findByIsActiveFalse();
+}
